17789de92ad7917596a9d7e1ba31d6a638aba24c
jhonatanchavezcastillo <jhonatanchavezcastillo@gmail.com>
2024-09-30 10:16:04 -0600

    Avance header y footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 //import Register from '../src/modules/plants/components/Register-step-2';
 //import Register from '../src/components/TaxonomicCatalog';
-import Register from '../src/modules/login/components/login';
+import Login from '../src/modules/login/components/login';
 
 function App() {
   return (
     <div className="App">
-      <Register/>
+      <Login/>
     </div>
   );
 }
